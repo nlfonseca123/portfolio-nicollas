6451b220636be96bdab73f884f81ff88ba6c59e2
nlfonseca123 <102531987+nlfonseca123@users.noreply.github.com>
2022-04-11 11:28:11 -0300

    atividade de numeros primos da aula do jackson

---
+++ atividadenumerosprimos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main
+   public static void main(strine[] args) {//Início do método main
+        Scanner bofao = new Scanner(System.in);//Criação do Scanner
+        System.out.println("Digite um número");//Mensagem para digitar algo
+        int n bofao.nextInt();//Variável 'n' recebe o valor digitado no Scanner
+       int i e;//Variável 'i' recebe o valor 'e'
+       while(i < n){//Enquanto i' for menor do que 'n', faça:
+            int j
+            int d
+           while(j > e){//Enquanto 'j' for maior do que 'e', faça:
+               if(i % j == 0){//Se o RESTO da divisão de 'i' por j' for 0, faça:
+                   i;//Variável 'j' recebe o valor da variável 'i'
+                   0;//Variável 'd' recebe o valor 'e'
+                   d++;//Adicione +1 ao valor da variável. 'd'
+               J//Fim do if
+               j--;//Subtrai -1 ao valor da variável 'j'
+           J//Fim do while
+           if(d ==
+               System.out.println(i);//Mensagem informando o valor de 'i'
+           }//Fim do if
+             i +;//Adiciona +1 ao valor da variável 'i'
+                   2){//Se o valor do variável 'd' for igual a '2', faço:
+       }//Fim do while
+    }//Fim do método main
