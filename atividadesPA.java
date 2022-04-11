242f1b97948eb2c72642f46c3632c1c6037630a8
nlfonseca123 <102531987+nlfonseca123@users.noreply.github.com>
2022-04-11 11:20:25 -0300

    anexado atividade de PA da aula do jackson

---
+++ atividadesPA.java
@@ -0,0 +1,13 @@
+public class Main
+   public static void sain(String ) args) {
+       int al- 151
+       int n 19;
+       int H;
+       int an al (n 1)r;
+       System.out.println("o velor dee An é:"an);
+       al (n 1)'r; an
+       Systee.out.printin("o valor dee Al é:"ra1);
+        r (an al)/(n-1):
+       Syste.out.printin("o valor dee R é:"r);
+       n ((an - al)/r)-1;
+       Systum.out.printin("O valor dee Né:"n);
